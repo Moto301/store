7e807b7801b481097da772434b7188c0e89933d0
moto301 <chrisoconnor9413@gmail.com>
2024-02-03 11:27:59 +1100

    product

--- components/products/ProductCard.js
+++ components/products/ProductCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function ProductCard({ permalink, image, name, description, price, soldOut}) {
+export default function ProductCard({ permalink, image, name, price, soldOut}) {
   return (
     <Link href="/product/[permalink]" as={`/product/${permalink}`}>
       <a className="mb-5 d-block font-color-black cursor-pointer">
@@ -17,7 +17,6 @@ export default function ProductCard({ permalink, image, name, description, price
         <p className="font-size-subheader mb-2 font-weight-medium">
           {name}
         </p>
-        <p className="mb-2 font-color-medium">{description}</p>
         <p className="font-size-subheader font-weight-medium pb-2 borderbottom border-color-black">
           {price}
         </p>
